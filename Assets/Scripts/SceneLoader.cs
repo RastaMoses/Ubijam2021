6739b730a3da8369afaeddbc4741d72e44643ce4
Leon <leon@nyoucomer.de>
2021-10-22 23:07:44 +0200

    SceneLoader added

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void ReloadLevel()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadNextLevel()
+    {
+        Debug.Log("Load Next Scene");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadMainMenu()
+    {
+
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+    
+    
+}
