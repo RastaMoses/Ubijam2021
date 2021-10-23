0d4fb4691c1015ee4ae3f2f79356aa02f064ddba
Johannes <johannes.leischmidt@web.de>
2021-10-23 13:35:09 +0200

    Mirror Teleport added

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    [SerializeField] bool activated;
+    [SerializeField] Teleport mirror2;
+    private Transform destination;
+    public float distance = 0.2f;
+    [SerializeField] float resetTimer = 0.5f;
+    bool waitForReset;
+
+    void Start()
+    {
+        
+            destination = mirror2.transform;
+        
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision");
+        if (!activated)
+        {
+            return;
+        }
+        
+        
+        else if (waitForReset)
+        {
+            Debug.Log("Wait for Reset");
+            
+        }
+        else if(Vector2.Distance(transform.position, collision.transform.position) > distance)
+
+        {
+            
+            Debug.Log("Teleport");
+            collision.transform.position = new Vector2(destination.position.x, destination.position.y);
+
+            mirror2.StartTimer();
+            
+        }
+    }
+
+    public void StartTimer()
+    {
+        StartCoroutine(ResetTimer());
+    }
+     IEnumerator ResetTimer()
+    {
+        waitForReset = true;
+        yield return new WaitForSeconds(resetTimer);
+        waitForReset = false;
+    }
+
+    public void SetActivated(bool activate)
+    {
+        activated = activate;
+    }
+
+    public void ActivateBoth()
+    {
+        activated = true;
+        mirror2.SetActivated(true);
+    }
+}
