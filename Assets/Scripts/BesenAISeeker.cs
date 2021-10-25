965b54baf1f6d0b59f9bb576e530204ece531085
Leon <leon@nyoucomer.de>
2021-10-25 20:08:35 +0200

    Fixxxes

--- Assets/Scripts/BesenAISeeker.cs
+++ Assets/Scripts/BesenAISeeker.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BesenAISeeker : MonoBehaviour
 {
-    [SerializeField] GameObject target;
+    GameObject target;
     public void SetTarget(Transform newTarget)
     {
         target = newTarget.gameObject;
